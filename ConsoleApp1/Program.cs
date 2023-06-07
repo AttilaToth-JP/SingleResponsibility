6a7a38b956019e5bde9316f8415abf2c34c5f506
atoth <atoth@scotcourts.gov.uk>
2023-06-07 13:12:42 +0100

    Changed the main class so that it would be responsible for only the programme flow, while separated its original responsibilities into new classes.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,35 +1,26 @@
-﻿//Code example by Tim Corey https://www.youtube.com/watch?v=5RwhyZnVRS8
+﻿//Code example taken from by Tim Corey's video https://www.youtube.com/watch?v=5RwhyZnVRS8
 
 
 using ConsoleApp1;
 
-Console.WriteLine("Welcome to my application!");
+StandardMessages.WelcomeMessage();
+
 
 //Ask for user information
-Person user  = new Person();
+Person user = PersonDataCapture.Capture();
 
-Console.WriteLine("What is your first name: ");
-user.FirstName = Console.ReadLine();
-
-Console.WriteLine("What is your last name: ");
-user.LastName = Console.ReadLine();
 
 //Checks to be sure the first and last names are valid
-if (string.IsNullOrWhiteSpace(user.FirstName))
+bool isUserValid = PersonValidator.Validate(user);
+
+if (isUserValid == false)
 {
-    Console.WriteLine("You didn't provide a valid first name!");
-    Console.ReadLine();
+    StandardMessages.EndApplication();
     return;
 }
 
-if (string.IsNullOrWhiteSpace(user.LastName))
-{
-    Console.WriteLine("You didn't provide a valid last name!");
-    Console.ReadLine();
-    return;
-}
 
 //Create a username for the person
-Console.WriteLine($"Your username is {user.FirstName.Substring(0, 1)}{user.LastName}");
+AccountGenerator.CreateAccount(user);
 
-Console.ReadLine();
+StandardMessages.EndApplication();
